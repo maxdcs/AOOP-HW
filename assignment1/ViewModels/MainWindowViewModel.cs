6cc26df632d2eec74ede38728a95b025341c5fad
maxdcs <maks.drzy@gmail.com>
2025-03-04 21:29:23 +0100

    Fixed bug where only the first string element can be toggled

--- assignment1/ViewModels/MainWindowViewModel.cs
+++ assignment1/ViewModels/MainWindowViewModel.cs
@@ -2,40 +2,56 @@
 using System.IO;
 using System.Windows.Input;
 
-
-namespace assignment1.ViewModels;
-
-public partial class MainWindowViewModel : ViewModelBase
+namespace assignment1.ViewModels
 {
-  public int X { get; }
-  public int Y { get; }
-
-  private string _values = string.Empty;
-  public string Values
-  {
-    get => _values;
-    set => SetProperty(ref _values, value);
-  }
-  public ICommand ToggleValueCommand { get; }
-
-  public MainWindowViewModel()
-  {
-    string[] lines = File.ReadAllLines("./Models/smile.b2img.txt");
-    string[] splitWords = lines[0].Split(' ');
-    X = Convert.ToInt32(splitWords[0]);
-    Y = Convert.ToInt32(splitWords[1]);
-    Values = lines[1];
-    ToggleValueCommand = new RelayCommand(_ => ToggleValue());
-  }
-
-  private void ToggleValue()
-  {
-    if (!string.IsNullOrEmpty(Values))
+    public partial class MainWindowViewModel : ViewModelBase
     {
-      char firstChar = Values[0];
-      char toggled = firstChar == '0' ? '1' : '0';
-      Values = toggled + (Values.Length > 1 ? Values.Substring(1) : "");
-    }
-  }
+        public int X { get; }
+        public int Y { get; }
 
+        private string _values = string.Empty;
+        public string Values
+        {
+            get => _values;
+            set => SetProperty(ref _values, value);
+        }
+
+        // Command expects a parameter (the index to toggle)
+        public ICommand ToggleValueCommand { get; }
+
+        public MainWindowViewModel()
+        {
+            // Read dimensions and values from file
+            string[] lines = File.ReadAllLines("./Models/smile.b2img.txt");
+            string[] splitWords = lines[0].Split(' ');
+            X = Convert.ToInt32(splitWords[0]);
+            Y = Convert.ToInt32(splitWords[1]);
+            Values = lines[1];
+
+            // Command uses the parameter (index) to toggle the appropriate character
+            ToggleValueCommand = new RelayCommand(param =>
+            {
+                if (param is string s && int.TryParse(s, out int index))
+                {
+                    ToggleValue(index);
+                }
+                else if (param is int indexInt)
+                {
+                    ToggleValue(indexInt);
+                }
+            });
+        }
+
+        private void ToggleValue(int index)
+        {
+            if (string.IsNullOrEmpty(Values) || index < 0 || index >= Values.Length)
+                return;
+            char current = Values[index];
+            char toggled = current == '0' ? '1' : '0';
+            // Create a new char array from the string, update the character, then assign a new string
+            char[] chars = Values.ToCharArray();
+            chars[index] = toggled;
+            Values = new string(chars);
+        }
+    }
 }
--- assignment1/obj/Debug/net9.0/assignment1.AssemblyInfo.cs
+++ assignment1/obj/Debug/net9.0/assignment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+84aa024716b89f37f2c6d6defb7cfb3aa028f3fb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a698eeecab8e8abfd47e4068e7d98f95a45e20e9")]
 [assembly: System.Reflection.AssemblyProductAttribute("assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
