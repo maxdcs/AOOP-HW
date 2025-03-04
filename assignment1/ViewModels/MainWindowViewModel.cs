495909aa249b46b35f8daf992caa1529d0310efa
mattveym <matej.tribula@gmail.com>
2025-03-04 17:23:49 +0100

    read the file from models and accessed it

--- assignment1/ViewModels/MainWindowViewModel.cs
+++ assignment1/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,33 @@
-﻿namespace assignment1.ViewModels;
+﻿using System;
+using System.IO;
+
+
+namespace assignment1.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
     public string Greeting { get; } = "Welcome to Avalonia!";
+
+    public string[] lines;
+
+    public string Item { get; }
+    public string Item2 { get; }
+    public string Testik { get; } = "blee";
+
+    public MainWindowViewModel()
+    {
+        // Read the file inside the constructor to ensure proper file handling
+        lines = File.ReadAllLines("./Models/smile.b2img.txt");
+        if (lines.Length > 0)
+        {
+            Item = lines[0]; // Set the Item property to the first line
+            Item2 = lines[1]; // Set the Item property to the first line
+        }
+        else
+        {
+            Item = "No lines found"; // Provide a default if file is empty
+            Item2 = "No lines found"; // Provide a default if file is empty
+        }
+    }
+
 }
--- assignment1/obj/Debug/net9.0/assignment1.AssemblyInfo.cs
+++ assignment1/obj/Debug/net9.0/assignment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+adce8aa1407e0768cb228b68fa898b9f5f069cc6")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5fcf697e8b8186df46368c95dcde3b97670050e3")]
 [assembly: System.Reflection.AssemblyProductAttribute("assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
