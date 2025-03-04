04ba88fa0b755eb78d6a449764e373a6af70e00a
nedasbartkus <nedbartkus@gmail.com>
2025-03-04 18:18:59 +0100

    Added file reading and attribute assignment

--- assignment1/ViewModels/MainWindowViewModel.cs
+++ assignment1/ViewModels/MainWindowViewModel.cs
@@ -1,6 +1,19 @@
-﻿namespace assignment1.ViewModels;
+﻿using System;
+using System.IO;
+
+namespace assignment1.ViewModels;
 
 public partial class MainWindowViewModel : ViewModelBase
 {
-    public string Greeting { get; } = "Welcome to Avalonia!";
+  public int X { get; }
+  public int Y { get; }
+  public string values { get; }
+
+  public MainWindowViewModel(){
+    string[] lines = File.ReadAllLines("./Models/smile.b2img.txt");
+    string[] splitWords = lines[0].Split(' ');
+    X = Convert.ToInt32(splitWords[0]);
+    Y = Convert.ToInt32(splitWords[1]);
+    values = lines[1];
+  }
 }
