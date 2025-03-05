2062597d927fdee26ff44ca858fd7f9e566f132e
maxdcs <maks.drzy@gmail.com>
2025-03-05 09:34:29 +0100

    removed gitignore from gitignore

--- assignment1/obj/Debug/net9.0/assignment1.AssemblyInfo.cs
+++ assignment1/obj/Debug/net9.0/assignment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("assignment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+16e0cc41a08cb2fed60d55bf451abf6e80062022")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+00f630b56f8c76d4e05f65730ae5274df065e5f7")]
 [assembly: System.Reflection.AssemblyProductAttribute("assignment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("assignment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
